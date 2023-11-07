d24152e0420599a332f27524b12919e412883a47
Felix <felix@pop-os.localdomain>
2023-11-07 10:46:45 +0100

    cut cond pdf

--- analysis/probabilities.py
+++ analysis/probabilities.py
@@ -4,24 +4,29 @@ import math
 import numpy as np
 
 
+# Kernel Density Estimator
 class KDE:
     def __init__(self, kernel='gaussian', bandwidth='silverman'):
         self.kernel = kernel
         self.bandwidth = bandwidth
 
 
+# K-Nearest Neighbor Estimator
 class KNN:
     def __init__(self, k=None):
         self.k = k
 
 
-def pdf_joint(estimator, returns):
+# probability density function Pr(X=x, Y=y, ...)
+# for list of return samples [[x1, y1, ...], [x2,y2, ...], ...]
+def pdf(estimator, returns):
     # use kernel density estimator
     if isinstance(estimator, KDE):
-        kde = KernelDensity(kernel=estimator.kernel, bandwidth=estimator.bandwidth).fit(returns)
+        kde = KernelDensity(kernel=estimator.kernel, bandwidth=estimator.bandwidth)
+        kde.fit(returns)
 
-        def density_function(*values):
-            log_density = kde.score([list(values)])
+        def density_function(values):
+            log_density = kde.score([values])
             density = np.exp(log_density)
             return density
 
@@ -34,34 +39,11 @@ def pdf_joint(estimator, returns):
         k = estimator.k if estimator.k else math.ceil(np.sqrt(n) / 2)
         knn = NearestNeighbors(n_neighbors=k).fit(returns)
 
-        def density_function(*values):
-            distances, _ = knn.kneighbors([list(values)])
+        def density_function(values):
+            distances, _ = knn.kneighbors([values])
             radius = np.max(distances)
             # volume of d-dimensional hypersphere
             volume = (np.pi ** (d / 2) / gamma(d / 2 + 1)) * (radius ** d)
-            return k / (n * volume)
-
-        return density_function
-
-
-def pdf(estimator, returns_joint, returns_cond=None):
-    if returns_cond is not None:
-        returns_all = np.hstack((returns_joint, returns_cond))
-
-        density_all = pdf_joint(estimator, returns_all)
-        density_cond = pdf_joint(estimator, returns_cond)
-
-        # Pr(A|B) = Pr(A,B) / Pr(B)
-        def density_function(*values):
-            # one value is no tuple, therefore list(values) errors
-            # values = [values] if not isinstance(values, tuple) else list(values)
-            return density_all(*values) / density_cond(*values[len(returns_all[0]) - len(returns_cond[0]):])
-
-        return density_function
-    else:
-        density = pdf_joint(estimator, returns_joint)
-
-        def density_function(*values):
-            return density(*values)
+            return (k - 1) / (n * volume)
 
         return density_function
