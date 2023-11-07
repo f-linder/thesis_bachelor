4027176ca3d5ce59a317ed9edae6cffdf23d9951
Felix <felix@pop-os.localdomain>
2023-11-07 10:48:36 +0100

    directed information estimate + test

--- analysis/information.py
+++ analysis/information.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def directed_information_tickers(ticker_source, ticker_target,
-                                 tickers_other=[], estimator=prob.KNN()):
+                                 tickers_cond=[], order=1, estimator=prob.KNN()):
     return 0
     # prepare data of tickers
     df_source = pd.read_csv(f'./data/{ticker_source}.csv')
@@ -23,45 +23,59 @@ def directed_information_tickers(ticker_source, ticker_target,
                                 returns_target.transpose(), cond, estimator)
 
 
-# I(X -> Y || Z) = 1/T sum_{t=1}^T I(X^t; Y^t | Z^t, X^t)
-# average mutual information over time horizon
-# TODO: ???
-def directed_information(source, target, cond=None, estimator=prob.KNN()):
-    pass
-#     T = len(source)
-#     di = 0.0
-#
-#     for t in range(500, T, 100):
-#         x = source[:t]
-#         y = target[:t]
-#         z = np.hstack ((cond[:t], y)) if cond is not None else None
-#
-#         mi = mutual_information(x, y, z)
-#         print(f'mi={mi}')
-#         di += mi
-#
-    # return di / ((T - 500) / 100)
+# estimation of directed information with memory of size l / order >= 1
+# I(X -> Y || Z) = 1/T Sum_t=1^T I(Y_t, X_t-l^t-1 | Y_t-l^t-1, Z_t-l^t-1)
+#                = 1/T Sum_t=1^T H(Y_t, Y_t-l^t-1, Z_t-l^t-1) + H(X_t-l^t-1, Y_t-l^t-1, Z_t-l^t-1)
+#                   - H(Y_t, Y_t-l^t-1, X_t-l^t-1 Z_t-l^t-1) - H(Y_t-l^t-1, Z_t-l^t-1)
+def directed_information(X, Y, Z=None, order=1, estimator=prob.KNN()):
+    Z = Z if Z is not None else [[] for _ in range(len(X) - order)]
 
+    # (X_t-l^t-1)
+    X_lagged = get_lagged_returns(X, [(1, order)])
+    # (Y_t-l^t-1)
+    Y_lagged = get_lagged_returns(Y, [(1, order)])
+    # (Z_t-l^t-1)
+    Z_lagged = get_lagged_returns(Z, [(1, order) for _ in range(len(Z[0]))])
 
-# based on Sricharan et al. (2011)
-# I(X;Y|Z) = H(X|Z) - H(X|Y,Z)
-#          = H(X,Z) + H(Y,Z) − H(X,Y,Z) − H(Z)
-#          = 1/N sum_{x,y,z} (log(Pr(x,y,z)) + log(Pr(z)) - log(Pr(x,z)) - log(Pr(y,z)) <- used
-def mutual_information(source, target, cond=None, estimator=prob.KNN()):
-    N = len(source)
-    cond_returns = cond if cond is not None else [[] for _ in range(N)]
+    ytyz = np.hstack((Y[order:], Y_lagged, Z_lagged))
+    xyz = np.hstack((X_lagged, Y_lagged, Z_lagged))
+    ytyxz = np.hstack((Y[order:], Y_lagged, X_lagged, Z_lagged))
+    yz = np.hstack((Y_lagged, Z_lagged))
 
-    xyz = np.hstack((source, target, cond_returns))
-    xz = np.hstack((source, cond_returns))
-    yz = np.hstack((target, cond_returns))
-
+    pdf_ytyz = prob.pdf(estimator, ytyz)
     pdf_xyz = prob.pdf(estimator, xyz)
-    pdf_z = prob.pdf(estimator, cond) if cond is not None else lambda *x: 1
-    pdf_xz = prob.pdf(estimator, xz)
+    pdf_ytyxz = prob.pdf(estimator, ytyxz)
     pdf_yz = prob.pdf(estimator, yz)
 
-    mi = 0.0
-    for x, y, *z in xyz:
-        mi += np.log2(pdf_xyz(x, y, *z)) + np.log2(pdf_z(*z)) - np.log2(pdf_xz(x, *z)) - np.log2(pdf_yz(y, *z))
+    T = len(X) - order
+    di = 0.0
 
-    return mi / N
+    for t in range(T):
+        # compute mutual information I(Y_t, X_t-l^t-1 | Y_t-l^t-1, Z_t-l^t-1)
+        mi = - np.log(pdf_ytyz(ytyz[t])) - np.log(pdf_xyz(xyz[t]))
+        mi += np.log(pdf_ytyxz(ytyxz[t])) + np.log(pdf_yz(yz[t]))
+        di += mi
+
+    return di / T
+
+
+# transforms list of return samples (X_t, Y_t, Z_t, ...) with corresponding
+# lag pairs = [(f0, l0), (f1, l1), (f2, l2), ...] with f_i < l_i
+# to (X_t-l0^t-f0, Y_t-l1^t-f2, Z_t-l2^t-f2, ...) or in python notation
+# (X[t-l0 : t-f0+1], Y[t-l1 : t-f1+1], Z[t-l2 : t-f2+1], ...)
+# lag pairs denote when in the past to start (t - l_i) and when to end (t - f_i)
+def get_lagged_returns(returns, lags):
+    n_features = len(returns[0])
+    max_lag = max([t[1] for t in lags]) if lags != [] else 0
+
+    if n_features != len(lags) or max_lag >= len(returns):
+        raise Exception('error get_lagged_returns()')
+
+    lagged_returns = [[] for _ in range(len(returns) - max_lag)]
+
+    # extract values of feature for each lag pair and add to lagged_returns
+    for feature, (first, furthest) in enumerate(lags):
+        lagged_feature = np.array([returns[max_lag - k: -k if k > 0 else None, feature] for k in range(first, furthest + 1)])
+        lagged_returns = np.hstack((lagged_returns, lagged_feature.transpose()))
+
+    return lagged_returns
