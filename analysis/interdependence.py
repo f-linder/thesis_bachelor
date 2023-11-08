e76e2e5e8701e7ba74b4b93ec9c24e501bae1205
Felix <felix@pop-os.localdomain>
2023-11-08 12:08:10 +0100

    implemented rolling window

--- analysis/interdependence.py
+++ analysis/interdependence.py
@@ -3,27 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def directed_information_tickers(ticker_source, ticker_target,
-                                 tickers_cond=[], order=1, estimator=prob.KNN()):
-    return 0
-    # prepare data of tickers
-    df_source = pd.read_csv(f'./data/{ticker_source}.csv')
-    df_target = pd.read_csv(f'./data/{ticker_target}.csv')
-    df_others = [pd.read_csv(f'./data/{o}.csv') for o in tickers_other]
-
-    returns_source = np.array(df_source['Returns'])
-    returns_target = np.array(df_target['Returns'])
-    returns_others = np.array([df['Returns'] for df in df_others])
-
-    # TODO: select subset of others with highest correlation
-    # corr_matrix = [np.corrcoef(returns_target, o) for o in returns_others]
-    cond = None
-
-    return directed_information(returns_source.transpose(),
-                                returns_target.transpose(), cond, estimator)
-
-
-# estimation of directed information with memory of size l / order >= 1
+# estimation of directed information with memory of size l (= order)
 # I(X -> Y || Z) = 1/T Sum_t=1^T I(Y_t, X_t-l^t-1 | Y_t-l^t-1, Z_t-l^t-1)
 #                = 1/T Sum_t=1^T H(Y_t, Y_t-l^t-1, Z_t-l^t-1) + H(X_t-l^t-1, Y_t-l^t-1, Z_t-l^t-1)
 #                   - H(Y_t, Y_t-l^t-1, X_t-l^t-1 Z_t-l^t-1) - H(Y_t-l^t-1, Z_t-l^t-1)
@@ -32,16 +12,16 @@ def directed_information(X, Y, Z=None, order=1, estimator=prob.KNN()):
 
     # (X_t-l^t-1)
     X_lagged = get_lagged_returns(X, [(1, order)])
-    # (Y_t-l^t-1)
     Y_lagged = get_lagged_returns(Y, [(1, order)])
-    # (Z_t-l^t-1)
     Z_lagged = get_lagged_returns(Z, [(1, order) for _ in range(len(Z[0]))])
 
+    # samples corresponding to entropy terms
     ytyz = np.hstack((Y[order:], Y_lagged, Z_lagged))
     xyz = np.hstack((X_lagged, Y_lagged, Z_lagged))
     ytyxz = np.hstack((Y[order:], Y_lagged, X_lagged, Z_lagged))
     yz = np.hstack((Y_lagged, Z_lagged))
 
+    # density funcitons corresponding to entropy terms
     pdf_ytyz = prob.pdf(estimator, ytyz)
     pdf_xyz = prob.pdf(estimator, xyz)
     pdf_ytyxz = prob.pdf(estimator, ytyxz)
@@ -52,11 +32,39 @@ def directed_information(X, Y, Z=None, order=1, estimator=prob.KNN()):
 
     for t in range(T):
         # compute mutual information I(Y_t, X_t-l^t-1 | Y_t-l^t-1, Z_t-l^t-1)
-        mi = - np.log(pdf_ytyz(ytyz[t])) - np.log(pdf_xyz(xyz[t]))
-        mi += np.log(pdf_ytyxz(ytyxz[t])) + np.log(pdf_yz(yz[t]))
+        mi = -np.log(pdf_ytyz(ytyz[t])) - np.log(pdf_xyz(xyz[t])) + np.log(pdf_ytyxz(ytyxz[t])) + np.log(pdf_yz(yz[t]))
         di += mi
 
     return di / T
+
+
+# assess time-varying directed information
+def rolling_window(window_size, step_size, X, Y, Z=None, order=1, estimator=prob.KNN()):
+    di = []
+    num_steps = (len(X) - window_size) // step_size
+
+    for i in range(num_steps):
+        start = i * step_size
+        end = start + window_size
+
+        window_X = X[start:end]
+        window_Y = Y[start:end]
+        window_Z = Z[start:end] if Z is not None else None
+
+        di_window = directed_information(window_X, window_Y, window_Z, order, estimator)
+        di.append(di_window)
+
+    # including samples otherwise cut off
+    start = num_steps * step_size
+
+    window_X = X[start:]
+    window_Y = Y[start:]
+    window_Z = Z[start:] if Z is not None else None
+
+    di_window = directed_information(window_X, window_Y, window_Z, order, estimator)
+    di.append(di_window)
+
+    return di
 
 
 # transforms list of return samples (X_t, Y_t, Z_t, ...) with corresponding
@@ -79,3 +87,25 @@ def get_lagged_returns(returns, lags):
         lagged_returns = np.hstack((lagged_returns, lagged_feature.transpose()))
 
     return lagged_returns
+
+
+def directed_information_tickers(ticker_source, ticker_target,
+                                 tickers_cond=[], order=1, estimator=prob.KNN()):
+    return 0
+    # prepare data of tickers
+    df_source = pd.read_csv(f'./data/{ticker_source}.csv')
+    df_target = pd.read_csv(f'./data/{ticker_target}.csv')
+    df_others = [pd.read_csv(f'./data/{o}.csv') for o in tickers_other]
+
+    returns_source = np.array(df_source['Returns'])
+    returns_target = np.array(df_target['Returns'])
+    returns_others = np.array([df['Returns'] for df in df_others])
+
+    # TODO: select subset of others with highest correlation
+    # corr_matrix = [np.corrcoef(returns_target, o) for o in returns_others]
+    cond = None
+
+    return directed_information(returns_source.transpose(),
+                                returns_target.transpose(), cond, estimator)
+
+
